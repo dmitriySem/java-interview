b76e9bb46738500dbb6e1996d938210839eda759
Dmitriy Semenov <dsemenov@saec.ru>
2024-12-04 12:28:30 +0500

    Пример Java streanApi

---
+++ JavaJunior/Lesson1_Lambda_StreamApi/src/macro/Main.java
@@ -0,0 +1,34 @@
+package macro;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        /*
+        Напишите программу, которая использует Stream API для обработки списка чисел.
+        Программа должна вывести на экран среднее значение всех четных чисел в списке.
+         */
+
+        List<Double> numbers = new ArrayList<>();
+
+        numbers.add(1.0);
+        numbers.add(2.1);
+        numbers.add(4.0);
+        numbers.add(5.0);
+        numbers.add(8.0);
+        numbers.add(48.0);
+
+        System.out.println();
+        OptionalDouble average = numbers.stream().filter(number -> number % 2 == 0).collect(Collectors.toList()).stream().mapToDouble(value -> value).average();
+        System.out.println(average.getAsDouble());
+
+
+    }
+}
