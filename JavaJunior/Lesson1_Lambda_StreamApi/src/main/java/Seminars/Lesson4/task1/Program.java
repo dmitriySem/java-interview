e956b8e58cacde541d00485fb4342867a5218a01
Dmitriy Semenov <dsemenov@saec.ru>
2024-12-12 11:41:21 +0500

    Семинар 4

--- JavaJunior/Lesson1_Lambda_StreamApi/src/main/java/Seminars/Lesson4/task1/Program.java
+++ JavaJunior/Lesson1_Lambda_StreamApi/src/main/java/Seminars/Lesson4/task1/Program.java
@@ -1,8 +1,138 @@
 package Seminars.Lesson4.task1;
 
-public class Program {
+import Seminars.Lesson4.task1.models.Student;
+import com.mysql.cj.xdevapi.PreparableStatement;
 
-    String url = "jdbc:mysql://localhost:3306/";
-    String user = "root";
-    String password = "1111";
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Random;
+
+public class Program {
+    private static Random random = new Random();
+
+
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/";
+        String user = "root";
+        String password = "1111";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+
+
+            createDatabase(connection);
+            System.out.println("Database created successfully");
+
+            useDatabase(connection);
+            System.out.println("Use database successfully");
+
+            createTable(connection);
+            System.out.println("Create table successfully");
+
+
+            int count = random.nextInt(5);
+//            System.out.println(count);
+            for (int i = 0; i < count; i++) {
+                Student student = Student.create();
+//                System.out.println(student);
+                insertData(connection, student);
+            }
+            System.out.println("Insert data successfully");
+
+            Collection<Student> students = readData(connection);
+            for (var student : students)
+                System.out.println(student);
+            System.out.println("Read data");
+
+            for (var student : students) {
+                student.updateName();
+                student.updateAge();
+                updateData(connection, student);
+            }
+            System.out.println("Update data");
+
+            students = readData(connection);
+            for (var student : students)
+                System.out.println(student);
+            System.out.println("Read data");
+
+            for (var student : students)
+                deleteData(connection, student.getId());
+            System.out.println("Delete data");
+
+            connection.close();
+            System.out.println("Database is closed successfully");
+
+        }
+    }
+
+    private static void insertData(Connection connection, Student student) throws SQLException {
+        String insertDataSQL = "INSERT INTO students (name, age) VALUES (?,?);";
+        try (PreparedStatement statement = connection.prepareStatement(insertDataSQL)){
+            statement.setString(1, student.getName());
+            statement.setInt(2, student.getAge());
+            statement.executeUpdate();
+        }
+    }
+
+
+
+
+    private static void createDatabase(Connection connection) throws SQLException {
+        String createDatabaseSQL = "CREATE DATABASE IF NOT EXISTS studentsDB;";
+        try (PreparedStatement statement = connection.prepareStatement(createDatabaseSQL)){
+            statement.execute();
+        }
+    }
+
+    private static void useDatabase(Connection connection) throws SQLException {
+        String useDatabaseSQL = "USE studentsDB;";
+        try (PreparedStatement statement = connection.prepareStatement(useDatabaseSQL)){
+            statement.execute();
+        }
+
+    }
+
+    private static void createTable(Connection connection) throws SQLException {
+        String createTableSQL = "CREATE TABLE IF NOT EXISTS students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), age INT);";
+        try (PreparedStatement statement = connection.prepareStatement(createTableSQL)){
+            statement.execute();
+        }
+    }
+
+    private static Collection<Student> readData(Connection connection) throws SQLException {
+        ArrayList<Student> studentList = new ArrayList<>();
+        String readDataSQL = "SELECT * FROM students;";
+        try (PreparedStatement statement = connection.prepareStatement(readDataSQL)){
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()){
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                studentList.add(new Student(id,name,age));
+            }
+            return studentList;
+        }
+    }
+
+    private static void updateData(Connection connection, Student student) throws SQLException {
+        String updateDataSQL = "UPDATE students SET name=?, age=? WHERE id=?;";
+        try (PreparedStatement statement = connection.prepareStatement(updateDataSQL)){
+            statement.setString(1, student.getName());
+            statement.setInt(2, student.getAge());
+            statement.setInt(3, student.getId());
+            statement.executeUpdate();
+        }
+    }
+
+    private static void deleteData(Connection connection, int id) throws SQLException {
+        String deteleDataSQL = "DELETE FROM students WHERE id = ?;";
+        try (PreparedStatement statement = connection.prepareStatement(deteleDataSQL)){
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        }
+    }
 }
+
+
+
---
+++ JavaJunior/Lesson1_Lambda_StreamApi/src/main/java/Seminars/Lesson4/task1/models/Student.java
@@ -0,0 +1,73 @@
+package Seminars.Lesson4.task1.models;
+
+
+import java.util.Random;
+
+public class Student {
+
+    private static final Random random = new Random();
+    private static final String[] names = new String[]{"Анатолий", "Владислав", "Владимир", "Дмитрий", "Александр", "Евгений"};
+
+    private int id;
+    private String name;
+    private int age;
+
+    public void updateAge(){
+        age = random.nextInt(26);
+    }
+
+    public void updateName(){
+        name = names[random.nextInt(names.length)];
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Student() {
+    }
+
+    public Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public static Student create(){
+        return new Student(names[random.nextInt(names.length)], random.nextInt(20));
+    }
+}
